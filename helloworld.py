0c21f209ac17c12912d0c8149382714faca71913
Lorem <22178748+Yaoeh@users.noreply.github.com>
2018-07-15 22:27:09 -0400

    made the code wayyy better

--- helloworld.py
+++ helloworld.py
@@ -1,4 +1,25 @@
+'''
+Making a bunch of errors!
+
+Adding
+
+Weird
+
+Unknown 
+
+Stuff
+
+and Text to an otherwise elegantly written program
+'''
+
+while True:
+	print('I am an infinite loop!')
 
 
+def infinite_recursion(func):
+	func(infinite_recursion)
 
-print "hello-world"
+if __name__=='__main__':
+	infinite_recursion(infinite_recursion)
+
+print("hello-world")
